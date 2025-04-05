ac1deef41080e9c0523ee7ee3cae908ddeefa8a5
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-05 00:33:17 -0400

    fix crash when guildStore fails to find

--- src/plugins/forceOwnerCrown/index.ts
+++ src/plugins/forceOwnerCrown/index.ts
@@ -35,14 +35,19 @@ export default definePlugin({
         }
     ],
     isGuildOwner(props: { user: User, channel: Channel, isOwner: boolean, guildId?: string; }) {
-        if (!props?.user?.id) return props.isOwner;
-        if (props.channel?.type === 3 /* GROUP_DM */)
-            return props.isOwner;
+        try {
+            if (!props?.user?.id) return props.isOwner;
+            if (props.channel?.type === 3 /* GROUP_DM */)
+                return props.isOwner;
 
-        // guild id is in props twice, fallback if the first is undefined
-        const guildId = props.guildId ?? props.channel?.guild_id;
-        const userId = props.user.id;
+            // guild id is in props twice, fallback if the first is undefined
+            const guildId = props.guildId ?? props.channel?.guild_id;
+            const userId = props.user.id;
 
-        return GuildStore.getGuild(guildId)?.ownerId === userId;
+            return GuildStore.getGuild(guildId)?.ownerId === userId;
+        } catch (e) {
+            console.error(e);
+            return props?.isOwner;
+        }
     },
 });
