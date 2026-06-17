1f1b0536b3748f49b92b007a04c33f1e3e7ab6bb
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-06-16 23:24:25 -0400

    GifMaker: Fix Naming Scheme

--- src/equicordplugins/gifMaker/index.tsx
+++ src/equicordplugins/gifMaker/index.tsx
@@ -5,7 +5,7 @@
  */
 
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
-import { definePluginSettings } from "@api/Settings";
+import { definePluginSettings, migratePluginSettings } from "@api/Settings";
 import { Button } from "@components/Button";
 import { EquicordDevs } from "@utils/constants";
 import { classNameFactory } from "@utils/css";
@@ -474,8 +474,9 @@ function openGifMakerFromItem(item) {
     ));
 }
 
+migratePluginSettings("GifMaker", "gifMaker");
 export default definePlugin({
-    name: "gifMaker",
+    name: "GifMaker",
     description: "Create and caption GIFs from any media in chat or the GIF picker.",
     authors: [EquicordDevs.Leon135, EquicordDevs.benjii],
     settings,
