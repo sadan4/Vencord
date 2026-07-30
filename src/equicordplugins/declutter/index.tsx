5a47e41f91efeff3c68bc95b39383daa70e24f08
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-07-30 03:11:11 -0400

    Declutter: Fix Missing Predicate

--- src/equicordplugins/declutter/index.tsx
+++ src/equicordplugins/declutter/index.tsx
@@ -184,6 +184,7 @@ export default definePlugin({
                 match: /(?=0!==\i.length&&\i\?\(0,)/,
                 replace: "null&&",
             },
+            predicate: () => settings.store.removeProfileFrame,
         },
         {
             // Avatar decoration
@@ -193,8 +194,7 @@ export default definePlugin({
                 replace: "$1null",
             },
             predicate: () =>
-                settings.store.removeAvatarDecoration &&
-                !isPluginEnabled(decor.name),
+                settings.store.removeAvatarDecoration && !isPluginEnabled(decor.name),
         },
         {
             // Avatar decoration on dms list
@@ -204,8 +204,7 @@ export default definePlugin({
                 replace: "null",
             },
             predicate: () =>
-                settings.store.removeAvatarDecoration &&
-                !isPluginEnabled(decor.name),
+                settings.store.removeAvatarDecoration && !isPluginEnabled(decor.name),
         },
         // User Area
         {
